e098b9a5e5c0522ab87b3e842097178d2a8e9675
birkin <birkin.diana@gmail.com>
2017-09-29 16:10:02 -0400

    updates env-settings prefix.

--- utils/simple_use_script.py
+++ utils/simple_use_script.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
-
 import os, pprint, time
 from bdpy import BorrowDirect
 
@@ -10,13 +8,13 @@ from bdpy import BorrowDirect
 
 print 'search example...'
 search_defaults = {
-    'API_URL_ROOT': unicode( os.environ['BDPY_SAMPLE_SCRIPT__API_URL_ROOT'] ),
-    'API_KEY': unicode( os.environ['BDPY_SAMPLE_SCRIPT__API_KEY'] ),
-    'PARTNERSHIP_ID': unicode( os.environ['BDPY_SAMPLE_SCRIPT__PARTNERSHIP_ID'] ),
-    'UNIVERSITY_CODE': unicode( os.environ['BDPY_SAMPLE_SCRIPT__UNIVERSITY_CODE'] )
+    'API_URL_ROOT': unicode( os.environ['BDPY3_SAMPLE_SCRIPT__API_URL_ROOT'] ),
+    'API_KEY': unicode( os.environ['BDPY3_SAMPLE_SCRIPT__API_KEY'] ),
+    'PARTNERSHIP_ID': unicode( os.environ['BDPY3_SAMPLE_SCRIPT__PARTNERSHIP_ID'] ),
+    'UNIVERSITY_CODE': unicode( os.environ['BDPY3_SAMPLE_SCRIPT__UNIVERSITY_CODE'] )
     }
 bd = BorrowDirect( search_defaults )
-patron_barcode = unicode( os.environ['BDPY_SAMPLE_SCRIPT__PATRON_BARCODE'] )
+patron_barcode = unicode( os.environ['BDPY3_SAMPLE_SCRIPT__PATRON_BARCODE'] )
 bd.run_search( patron_barcode, 'ISBN', '9780688002305' )
 print 'search_result...'; pprint.pprint( bd.search_result )
 
@@ -28,13 +26,13 @@ time.sleep( 1 )  # being nice to the server
 print 'request example...'
 ## Will really generate request if item is requestable
 request_defaults = {
-    'API_URL_ROOT': unicode( os.environ['BDPY_SAMPLE_SCRIPT__API_URL_ROOT'] ),
-    'API_KEY': unicode( os.environ['BDPY_SAMPLE_SCRIPT__API_KEY'] ),
-    'PARTNERSHIP_ID': unicode( os.environ['BDPY_SAMPLE_SCRIPT__PARTNERSHIP_ID'] ),
-    'UNIVERSITY_CODE': unicode( os.environ['BDPY_SAMPLE_SCRIPT__UNIVERSITY_CODE'] ),
-    'PICKUP_LOCATION': unicode( os.environ['BDPY_SAMPLE_SCRIPT__PICKUP_LOCATION'] )
+    'API_URL_ROOT': unicode( os.environ['BDPY3_SAMPLE_SCRIPT__API_URL_ROOT'] ),
+    'API_KEY': unicode( os.environ['BDPY3_SAMPLE_SCRIPT__API_KEY'] ),
+    'PARTNERSHIP_ID': unicode( os.environ['BDPY3_SAMPLE_SCRIPT__PARTNERSHIP_ID'] ),
+    'UNIVERSITY_CODE': unicode( os.environ['BDPY3_SAMPLE_SCRIPT__UNIVERSITY_CODE'] ),
+    'PICKUP_LOCATION': unicode( os.environ['BDPY3_SAMPLE_SCRIPT__PICKUP_LOCATION'] )
     }
 bd = BorrowDirect( request_defaults )
-patron_barcode = unicode( os.environ['BDPY_SAMPLE_SCRIPT__PATRON_BARCODE'] )
+patron_barcode = unicode( os.environ['BDPY3_SAMPLE_SCRIPT__PATRON_BARCODE'] )
 bd.run_request_item( patron_barcode, 'ISBN', '9780688002305' )
 print 'request_result...'; pprint.pprint( bd.request_result )
