de31e522ac066005c77387892f45bdc15fadfcf2
birkin <birkin.diana@gmail.com>
2017-10-06 11:47:21 -0400

    updates function name to make way for similar one.

--- bdpy3/request.py
+++ bdpy3/request.py
@@ -19,11 +19,12 @@ class Requester( object ):
         self.valid_search_types = [ 'ISBN', 'ISSN', 'LCCN', 'OCLC', 'PHRASE' ]
 
     def request_exact_item( self, patron_barcode, search_type, search_value, pickup_location, api_url_root, api_key, partnership_id, university_code ):
-        """ Searches for exact key-value.
+        """ Runs an 'ExactSearch' query.
+            <https://relais.atlassian.net/wiki/spaces/ILL/pages/106608984/RequestItem#RequestItem-RequestItemrequestjson>
             Called by BorrowDirect.run_request_exact_item() """
         assert search_type in self.valid_search_types
         authorization_id = self.get_authorization_id( patron_barcode, api_url_root, api_key, partnership_id, university_code )
-        params = self.build_params( partnership_id, authorization_id, pickup_location, search_type, search_value )
+        params = self.build_exact_search_params( partnership_id, authorization_id, pickup_location, search_type, search_value )
         url = '%s/dws/item/add?aid=%s' % ( api_url_root, authorization_id )
         headers = { 'Content-type': 'application/json' }
         r = requests.post( url, data=json.dumps(params), headers=headers )
@@ -42,7 +43,7 @@ class Requester( object ):
             patron_barcode, api_url_root, api_key, partnership_id, university_code )
         return authorization_id
 
-    def build_params( self, partnership_id, authorization_id, pickup_location, search_type, search_value ):
+    def build_exact_search_params( self, partnership_id, authorization_id, pickup_location, search_type, search_value ):
         """ Builds request json.
             Called by request_exact_item() """
         params = {
@@ -52,9 +53,9 @@ class Requester( object ):
             'ExactSearch': [ {
                 'Type': search_type,
                 'Value': search_value
-                } ]
+                } ]`
             }
         log.debug( 'params, `%s`' % pprint.pformat(params) )
         return params
 
-    # end class Requester
+    ## end class Requester()
