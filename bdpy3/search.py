7e37ff822737a2b3e4f8894ef409f485ce6c41f4
birkin <birkin.diana@gmail.com>
2017-10-19 13:38:02 -0400

    removes commented-out code.

--- bdpy3/search.py
+++ bdpy3/search.py
@@ -18,20 +18,6 @@ class Searcher( object ):
     def __init__( self ):
         self.valid_search_types = [ 'ISBN', 'ISSN', 'LCCN', 'OCLC', 'PHRASE' ]
 
-    # def search( self, patron_barcode, search_type, search_value, api_url_root, api_key, partnership_id, university_code ):
-    #     """ Searches for exact key-value.
-    #         Called by BorrowDirect.run_search() """
-    #     assert search_type in self.valid_search_types
-    #     authorization_id = self.get_authorization_id( patron_barcode, api_url_root, api_key, university_code, partnership_id )
-    #     params = self.build_params( partnership_id, university_code, patron_barcode, search_type, search_value )
-    #     url = '%s/dws/item/available?aid=%s' % ( api_url_root, authorization_id )
-    #     headers = { 'Content-type': 'application/json' }
-    #     r = requests.post( url, data=json.dumps(params), headers=headers, timeout=90 )
-    #     log.debug( 'search r.url, `%s`' % r.url )
-    #     log.debug( 'search r.content, `%s`' % r.content.decode('utf-8') )
-    #     result_dct = r.json()
-    #     return result_dct
-
     def search_exact_item( self, patron_barcode, api_url_root, api_key, partnership_id, university_code, search_type, search_value ):
         """ Searches for exact key-value.
             Called by BorrowDirect.run_search() """
@@ -47,17 +33,6 @@ class Searcher( object ):
         result_dct = r.json()
         return result_dct
 
-    # def get_authorization_id( self, patron_barcode, api_url_root, api_key, partnership_id, university_code ):
-    #     """ Obtains authorization_id.
-    #         Called by search()
-    #         Note that only the authenticator webservice is called;
-    #           the authorization webservice simply extends the same id's session time and so is not needed here. """
-    #     log.debug( 'starting get_authorization_id()...' )
-    #     authr = Authenticator()
-    #     authorization_id = authr.authenticate(
-    #         patron_barcode, api_url_root, api_key, university_code, partnership_id )
-    #     return authorization_id
-
     def get_authorization_id( self, patron_barcode, api_url_root, api_key, partnership_id, university_code ):
         """ Obtains authorization_id.
             Called by search()
@@ -68,17 +43,6 @@ class Searcher( object ):
         authorization_id = authr.authenticate(
             patron_barcode, api_url_root, api_key, partnership_id, university_code )
         return authorization_id
-
-    # def build_params( self, partnership_id, university_code, patron_barcode, search_type, search_value ):
-    #     """ Builds search json.
-    #         Called by search() """
-    #     params = {
-    #         'PartnershipId': partnership_id,
-    #         'ExactSearch': [ {
-    #             'Type': search_type, 'Value': search_value } ]
-    #             }
-    #     log.debug( 'params, `%s`' % pprint.pformat(params) )
-    #     return params
 
     def build_exact_item_params( self, patron_barcode, partnership_id, university_code, search_type, search_value ):
         """ Builds search json.
